93ad6006ae96eedd7812cfe42accbd8c2a258e79
Robin Kardell <carden.rk@gmail.com>
2024-12-15 17:38:57 +0100

    sum

--- models/brand_service.py
+++ models/brand_service.py
@@ -7,8 +7,8 @@ class BrandService(models.Model):
 
     name = fields.Char(string='Service Reference', required=True, copy=False, readonly=True, default='New')
     date = fields.Date(string='Service Date', default=fields.Date.today)
-    customer_id = fields.Many2one('res.partner', string='Customer', required=True)
-    product_id = fields.Many2one('product.product', string='Product', required=True)
+    customer_id = fields.Many2one('res.partner', string='Customer', required=True, tracking=True)
+    product_id = fields.Many2one('product.product', string='Product', required=True, tracking=True)
     location = fields.Char(string='Location')
     serial_number = fields.Char(string='Serial Number')
     state = fields.Selection([
@@ -22,14 +22,8 @@ class BrandService(models.Model):
         ('repair', 'Repair'),
         ('maintenance', 'Maintenance'),
         ('inspection', 'Inspection')
-    ], string='Service Type', required=True)
+    ], string='Service Type', required=True, default='maintenance')
     technician_id = fields.Many2one('res.users', string='Technician', default=lambda self: self.env.user)
-    action = fields.Selection([
-        ('new', 'New'),
-        ('service', 'Service'),
-        ('replace', 'Replacement'),
-        ('none', 'No Action')
-    ], string='Action', default='new')
 
     @api.model
     def create(self, vals):
